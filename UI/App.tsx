118d10209a15e098aa093b5caf81cd602bb0d143
Abyel <abyel26@gmail.com>
2024-05-26 08:58:24 -0600

    comitting

--- UI/App.tsx
+++ UI/App.tsx
@@ -77,7 +77,7 @@ function App(): React.JSX.Element {
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
           <Section title="Step One">
-            Hola Abejas!
+            Hola Abejas 2!
           </Section>
           <Section title="See Your Changes">
             <ReloadInstructions />
